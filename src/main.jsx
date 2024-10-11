a1f8a8b68cabce4a13a63a5a358578283ec56482
Guada8a <jochoa10@ucol.mx>
2024-10-11 11:56:08 -0600

    chore: actualizacion de configuracion

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,35 @@
-import DashboardCard from "./components/DashboardCard"
+// src/main.jsx
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { Button, Input, DatePicker } from 'antd'
+import PropTypes from 'prop-types';
+import 'antd/dist/reset.css'
+import DashboardCard from './components/DashboardCard'
 
-export default DashboardCard
+const MiBoton = ({ children, ...props }) => (
+  <Button type="primary" {...props}>{children}</Button>
+)
+
+const MiInput = (props) => (
+  <Input {...props} />
+)
+
+const MiDatePicker = (props) => (
+  <DatePicker {...props} />
+)
+
+const render = (containerId, Component) => {
+  ReactDOM.createRoot(document.getElementById(containerId)).render(
+    <React.StrictMode>
+      <Component />
+    </React.StrictMode>
+  )
+}
+
+// Exporta los componentes y la función de renderizado
+export { MiBoton, MiInput, MiDatePicker, DashboardCard, render }
+
+// Define los tipos de las propiedades
+MiBoton.propTypes = {
+  children: PropTypes.node
+}
